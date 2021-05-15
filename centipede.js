a2d950204d8ece41e0593a93ca0c0a093fdfc483
waterboyWSL <randyrec30@gmail.com>
2021-05-15 04:03:02 -0400

    Added shooting.

--- centipede.js
+++ centipede.js
@@ -53,6 +53,17 @@ function keyUpHandler(e) {
 }
 
 var player = new Player();
+var shot = new Bullet(player.x, player.y);
+var mushs = [];
+var trains = [];
+//list of centipedes
+//centipedes
+//head node
+//rest nodes
+//update
+//  check all nodes
+//  check if node broken and make new centipede if needed
+//draw
 
 //Mushrooms-x, y, damage
 //Cnetipede-x, y, damage (update xy check collision)
@@ -64,33 +75,46 @@ function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	player.update();
 	player.draw();
+    shot.update();
+    shot.draw();
+    //trains update draw
+    //mushs update draw
 	//window.requestAnimationFrame(draw);
 }
 setInterval(draw, 1);
 //window.requestAnimationFrame(draw);
 
 
+function shoot(player) {
+    if (!shot.visible) {
+        shot.x = player.x;
+        shot.y = player.y;
+        shot.visible = true;
+    }
+}
+
+
 
 function Entity(xdraw, ydraw, health, damage, priority) {
-	this.x = 0; 
-	this.y = 0;
-	this.xdraw = xdraw;
-	this.ydraw = ydraw;
-	this.health = health;
-	this.damage = damage;
-	this.priority = priority;
-	this.collides = function (entity) {
-		mex = [this.x - this.xdraw, this.x + this.xdraw];
-		mey = [this.y - this.ydraw, this.y + this.ydraw];
-		themx = [entity.x - entity.xdraw, entity.x + entity.xdraw];
-		themy = [entity.y - entity.ydraw, entity.y + entity.ydraw];
-		if (themx[0] <= mex[1] && themx[1] >= mex[0]) {
-			if (themy[0] <= mey[1] && themy[1] >= mey[0]) {
-				return true;
-			}
-		}
-		return false;
-	};
+    this.x = 0; 
+    this.y = 0;
+    this.xdraw = xdraw;
+    this.ydraw = ydraw;
+    this.health = health;
+    this.damage = damage;
+    this.priority = priority;
+    this.collides = function (entity) {
+        mex = [this.x - this.xdraw, this.x + this.xdraw];
+        mey = [this.y - this.ydraw, this.y + this.ydraw];
+        themx = [entity.x - entity.xdraw, entity.x + entity.xdraw];
+        themy = [entity.y - entity.ydraw, entity.y + entity.ydraw];
+        if (themx[0] <= mex[1] && themx[1] >= mex[0]) {
+            if (themy[0] <= mey[1] && themy[1] >= mey[0]) {
+                return true;
+            }
+        }
+        return false;
+    };
 }
 
 function Player() {
@@ -118,13 +142,34 @@ function Player() {
 			this.y += 1;
 		}
 		if (shootPressed) {
-			
+			shoot(this);
 		}
-	}
+	};
 }
 
-function Bullet() {
-	Entity.call(this, 5, 5, 1, 1, 2);
+function Bullet(x, y) {
+	Entity.call(this, 0, 10, 1, 1, 2);
+    this.x = x;
+    this.y = y;
+    this.visible = false;
+    this.draw = function() {
+        if (this.visible) {
+            ctx.strokeStyle = 'red';
+            ctx.lineWidth = 1;
+            ctx.beginPath();
+            ctx.moveTo(this.x, this.y + this.ydraw);
+            ctx.lineTo(this.x, this.y - this.ydraw);
+            ctx.stroke();
+        }
+    };
+    this.update = function() {
+        if (this.visible) {
+            if (this.y <= 0) {
+                this.visible = false;
+            }
+            this.y -= (this.ydraw/2);
+        }
+    };
 }
 
 function Mushroom() {
